bb0dc921c00bd48f6a0e9a96c1d96fc50dc4038a
Nikolai Li <122288063+NikolaiLi@users.noreply.github.com>
2025-09-11 13:11:46 +0200

    Added pseudo goroutine and channel to Philosopher

--- DiningTable.go
+++ DiningTable.go
@@ -1,9 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	for i := 0; i <= 5; i++ {
+	for i := 1; i <= 5; i++ {
 		philosopher(i, false, false)
 		fork(i, 0)
 	}
@@ -11,9 +9,12 @@ func main() {
 
 func philosopher(id int, right bool, left bool) {
 	if right && left == false {
-		fmt.Println("Taking Fork")
-		right = false
-		left = false
+		message := make(chan string)
+		go func() { message <- "request" }()
+
+		// Take both forks
+		right = true
+		left = true
 	}
 }
 
