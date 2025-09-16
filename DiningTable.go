ad02b5941cb9f2e173ac78d4d7d8f67811c26a70
Nikolai Li <122288063+NikolaiLi@users.noreply.github.com>
2025-09-16 13:59:33 +0200

    Næsten færdig program
    - Main stopper stadig
    - Deadlocks skal fixes

--- DiningTable.go
+++ DiningTable.go
@@ -2,33 +2,61 @@ package main
 
 func main() {
 	forkChannels := make([]chan Request, 5)
-	for i := 0; i < 5; i++ {
-		forkChannels[i] = make(chan Request)
-		go fork(i, forkChannels[i])
+
+	counter := make([]int, 5)
+	counterFlag := make([]bool, 5)
+
+	for i := 0; i < len(counter); i++ {
+		println("Philosopher", i, "is thinking")
+		if counter[i] >= 3 {
+			counterFlag[i] = true
+		} else {
+			counterFlag[i] = false
+		}
 	}
 
-	for i := 0; i < 5; i++ {
-		left := i
-		right := (i + 1) % 5
-		go philosopher(i, forkChannels[left], forkChannels[right])
+	for i := 0; i < len(counterFlag); i++ {
+		if counterFlag[i] == false {
+			for i := 0; i < 5; i++ {
+				forkChannels[i] = make(chan Request)
+				go fork(i, forkChannels[i])
+			}
+
+			for i := 0; i < 5; i++ {
+				left := i
+				right := (i + 1) % 5
+				go philosopher(i, forkChannels[left], forkChannels[right], counter)
+			}
+		}
 	}
 }
 
-func philosopher(id int, left chan Request, right chan Request) {
+func philosopher(id int, left chan Request, right chan Request, counter []int) {
 	replyChan := make(chan bool)
 
-	left <- Request{philosopherID: id, action: "take", reply: replyChan}
-	<-replyChan
+	for counter[id] < 3 {
+		if id != 4 {
+			left <- Request{philosopherID: id, action: "take", reply: replyChan}
+			<-replyChan
 
-	right <- Request{philosopherID: id, action: "take", reply: replyChan}
-	<-replyChan
+			right <- Request{philosopherID: id, action: "take", reply: replyChan}
+			<-replyChan
+		} else {
+			right <- Request{philosopherID: id, action: "take", reply: replyChan}
+			<-replyChan
 
-	println("Philosopher", id, "is eating")
+			left <- Request{philosopherID: id, action: "take", reply: replyChan}
+			<-replyChan
+		}
 
-	left <- Request{philosopherID: id, action: "release", reply: replyChan}
-	right <- Request{philosopherID: id, action: "release", reply: replyChan}
+		println("Philosopher", id, "is eating")
+		counter[id]++
 
-	println("Philosopher", id, "is thinking")
+		left <- Request{philosopherID: id, action: "release", reply: replyChan}
+		right <- Request{philosopherID: id, action: "release", reply: replyChan}
+
+		println("Philosopher", id, "is thinking")
+	}
 }
 
 func fork(id int, requests chan Request) {
